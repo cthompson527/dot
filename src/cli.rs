8ea6fef691c3a0b50cc21eb154eb25085cd605bf
Cory Thompson <cory.thompson527@gmail.com>
2022-12-25 08:55:54 -0600

    :package: Create a library crate for git

---
+++ src/cli.rs
@@ -0,0 +1,41 @@
+use clap::{Parser, Subcommand};
+
+/// Automatic dotfile configuration and backup using git
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[command(subcommand)]
+    pub action: Action
+}
+    /// Setup the dotfile folder by cloning from the repo
+
+#[derive(Subcommand, Debug)]
+pub enum Action {
+    /// Setup the dotfile folder by cloning from the repo
+    Init {
+        /// URL to repository
+        url: String,
+    },
+
+    /// Create a new commit and push to the repo
+    Push {
+        /// Branch to commit and push to
+        #[arg(default_value = "main")]
+        branch: String,
+
+        /// Commit message
+        #[arg(short, long)]
+        message: Option<String>,
+    },
+
+    /// Pull the changes from the repo and merge into current branch
+    Pull {
+        /// Branch to pull from
+        #[arg(default_value = "main")]
+        branch: String
+    }
+}
+
+pub fn parse() -> crate::cli::Args {
+    Args::parse()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,18 @@
-use clap::{Parser, Subcommand};
 use chrono::prelude::*;
+use dot::git;
 
-mod git;
-
-/// Automatic dotfile configuration and backup using git
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    #[command(subcommand)]
-    action: Action
-}
-    /// Setup the dotfile folder by cloning from the repo
-
-#[derive(Subcommand, Debug)]
-enum Action {
-    /// Setup the dotfile folder by cloning from the repo
-    Init {
-        /// URL to repository
-        url: String,
-    },
-
-    /// Create a new commit and push to the repo
-    Push {
-        /// Branch to commit and push to
-        #[arg(default_value = "main")]
-        branch: String,
-
-        /// Commit message
-        #[arg(short, long)]
-        message: Option<String>,
-    },
-
-    /// Pull the changes from the repo and merge into current branch
-    Pull {
-        /// Branch to pull from
-        #[arg(default_value = "main")]
-        branch: String
-    }
-}
+mod cli;
 
 fn main() {
-    let cli = Args::parse();
-    println!("{:?}", cli);
+    let args = cli::parse();
 
-    match &cli.action {
-        Action::Init { url } => {
+    match &args.action {
+        cli::Action::Init { url } => {
             println!("Pulling from {}", url);
             git::gclone(url.to_string());
 
         },
-        Action::Push { branch, message } => {
+        cli::Action::Push { branch, message } => {
             println!("Pushing to {}", branch);
             let msg = match message {
                 Some(m) => m.to_owned(),
@@ -57,11 +20,9 @@ fn main() {
             };
             git::gpush(branch.to_string(), msg);
         },
-        Action::Pull { branch } => {
+        cli::Action::Pull { branch } => {
             println!("Pulling from {}", branch);
             git::gpull(branch.to_string());
         },
     }
-
-    // println!("{:?}", cli);
 }
