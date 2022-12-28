d063c297fdc6b1ed4d718bfea794ea97e53a0cd5
Cory Thompson <cory.thompson527@gmail.com>
2022-12-27 22:33:23 -0600

    :white_check_mark: Tests for dotfiles dir

--- src/lib/git.rs
+++ src/lib/git.rs
@@ -27,7 +27,7 @@ macro_rules! git {
 fn dotfiles_dir() -> String {
     let home = match env::var("HOME") {
         Ok(path) => path,
-        Err(_) => "/".to_string(),
+        Err(_) => "".to_string(),
     };
     format!("{}/dotfiles", home)
 }
@@ -46,4 +46,21 @@ pub fn gpush(branch: String, message: String) {
 pub fn gpull(branch: String) {
     let cdir = dotfiles_dir();
     git!(dir = cdir; "pull", "origin", format!("{}:main", branch));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dotfiles_dir_when_home_env_var_is_set() {
+        env::set_var("HOME", "/home/apollo");
+        assert_eq!(dotfiles_dir(), "/home/apollo/dotfiles");
+    }
+
+    #[test]
+    fn test_dotfiles_dir_when_home_env_var_is_not_set() {
+        env::remove_var("HOME");
+        assert_eq!(dotfiles_dir(), "/dotfiles");
+    }
 }
